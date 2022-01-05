d516edc8c4eef770e711ee6a8842cea0ed6e94c7
Arturo0911 <anegreiross@outlook.com>
2022-01-05 13:01:16 -0500

    ok

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,6 @@
 from app import create_app
-import os
 
 
 if __name__ == "__main__":
-    print("VERIFYING......")
-    print(os.system("ls -la"))
     app = create_app()
     app.run(host="0.0.0.0", port=5000)
