a5fad1444ae90f0073562216f895806c840af931
Arturo0911 <anegreiross@outlook.com>
2022-01-09 20:11:52 -0500

    finished the client part initializing

---
+++ client/src/components/Home.vue
@@ -0,0 +1,6 @@
+5 lines (5 sloc)  89 Bytes
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
---
+++ client/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App.vue'
+
+import router from './router/routes'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+Vue.use(VueRouter)
+Vue.use(IconsPlugin)
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+Vue.config.productionTip = false
+
+//const router = new VueRouter({ routes })
+
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
